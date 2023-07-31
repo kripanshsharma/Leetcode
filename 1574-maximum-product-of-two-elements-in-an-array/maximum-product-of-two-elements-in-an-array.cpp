7074f6dfcbc5fa2cdf4bb6af64888fe7525fd3bb
Kripansh Sharma <72396861+kripanshsharma@users.noreply.github.com>
2023-08-01 00:42:40 +0530

    Time: 4 ms (77.57%) | Memory: 10 MB (44.15%) - LeetSync

--- 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.cpp
+++ 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.cpp
@@ -15,4 +15,14 @@ public:
         int mul = (nums[n]-1)*(nums[n-1]-1);
         return mul;
     }
+    /*int max1=0 , max2=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] >= max1){
+                max2 = max1;
+                max1 = nums[i];
+            }
+            else max2 = Math.max(max2, nums[i]);
+        } 
+        return (max1-1)*(max2-1);
+        */
 };
