3f264dd23fc3384c08547e2216d97b48d84eaecb
Kripansh Sharma <72396861+kripanshsharma@users.noreply.github.com>
2023-08-03 23:36:26 +0530

    Time: 1 ms (46.85%) | Memory: 5.9 MB (39.78%) - LeetSync

--- 172-factorial-trailing-zeroes/factorial-trailing-zeroes.cpp
+++ 172-factorial-trailing-zeroes/factorial-trailing-zeroes.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int trailingZeroes(int n) {
         int count = 0 ;
-        for (int i = 5; n / i >= 1; i *= 5){
+        for (int i = 5; i<=n; i *= 5){
             count += n / i;
         }
         return count;
