8d1bc425a1f7a6e5fbaf5f9d6bfd35e4941861d7
Kripansh Sharma <72396861+kripanshsharma@users.noreply.github.com>
2023-08-10 01:07:07 +0530

    Time: 15 ms (84.70%) | Memory: 19.5 MB (55.46%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,10 +1,21 @@
 class Solution {
 public:
-
+    /*
     int majorityElement(vector<int>& nums) {
         int n = nums.size();
         sort(nums.begin(), nums.end());
         return nums[n/2];
         
+    }*/
+        int majorityElement(vector<int>& nums) {
+        int ctr=1;
+        int ele=nums[0];
+
+        for(int i=1;i<nums.size();i++){
+            if(!ctr) ele=nums[i];
+
+            ele==nums[i]?ctr++:ctr--;
+        }
+        return ele;
     }
 };
