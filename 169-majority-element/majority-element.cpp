e127726720b7bfb362ddf9a8b39dc95f9b971ae5
Kripansh Sharma <72396861+kripanshsharma@users.noreply.github.com>
2023-08-10 00:40:08 +0530

    Time: 15 ms (85.55%) | Memory: 19.5 MB (54.91%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,28 +1,9 @@
 class Solution {
 public:
-void insertionSort(int n, vector<int> &arr){
-    
-    for(int i = 1; i<n; i++) {
-        int temp = arr[i];
-        int j = i-1;
-        for(; j>=0; j--) {
-            
-            if(arr[j] > temp) {
-                //shift
-                arr[j+1] = arr[j];
-            }
-            else { // ruk jao
-                break;
-            }
-            
-        }
-        //copy temp value
-        arr[j+1] = temp;  
-    } 
-}
+
     int majorityElement(vector<int>& nums) {
         int n = nums.size();
-        insertionSort(n, nums);
+        sort(nums.begin(), nums.end());
         return nums[n/2];
         
     }
