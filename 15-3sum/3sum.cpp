41fb94e0fe21f703846dd54c348811f91d155596
Kripansh Sharma <72396861+kripanshsharma@users.noreply.github.com>
2023-08-16 02:09:33 +0530

    Time: 116 ms (86.27%) | Memory: 25.9 MB (59.26%) - LeetSync

---
+++ 15-3sum/3sum.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+
+        int n = nums.size();
+        vector<vector<int>> ans;
+        sort(nums.begin(), nums.end());
+        for(int i=0; i<n; i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            int j = i+1;
+            int k = n-1;
+            while(j<k){
+                int sum = nums[i]+nums[j]+nums[k];
+                if(sum<0){
+                    j++;
+                }
+                else if(sum>0){
+                    k--;
+                }
+                else{
+                    vector<int> ele = {nums[i], nums[j], nums[k]};
+                    ans.push_back(ele);
+                    j++;
+                    k--;
+                    while(j<k && nums[j]==nums[j-1]) j++;
+                    while(j<k && nums[k]==nums[k+1]) k--;//logic to check repeated pairs
+                    //ek element ko ek hi baaar lena varna same st banega
+                }
+            }
+        }
+        return ans;
+
+
+    }
+};
