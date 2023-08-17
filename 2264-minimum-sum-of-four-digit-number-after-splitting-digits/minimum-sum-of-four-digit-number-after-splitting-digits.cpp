115f45934f943678b035304205dfee70128e10c8
Kripansh Sharma <72396861+kripanshsharma@users.noreply.github.com>
2023-08-18 04:13:16 +0530

    Time: 3 ms (25.18%) | Memory: 6 MB (58.87%) - LeetSync

--- 2264-minimum-sum-of-four-digit-number-after-splitting-digits/minimum-sum-of-four-digit-number-after-splitting-digits.cpp
+++ 2264-minimum-sum-of-four-digit-number-after-splitting-digits/minimum-sum-of-four-digit-number-after-splitting-digits.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
     int minimumSum(int num) {
-        vector<int> arr;
-        while(num!=0){
-            arr.push_back(num%10);
-            num = num/10;
-        }
-        sort(arr.begin(), arr.end());
-        int sum1 = (10*arr[0] + arr[2]) + (10*arr[1] + arr[3]);
-        int sum2 = (10*arr[0] + arr[3]) + (10*arr[1] + arr[2]);
-        int res = min(sum1, sum2);
+        // vector<int> arr;
+        // while(num!=0){
+        //     arr.push_back(num%10);
+        //     num = num/10;
+        // }
+        // sort(arr.begin(), arr.end());
+        // int sum1 = (10*arr[0] + arr[2]) + (10*arr[1] + arr[3]);
+        // int sum2 = (10*arr[0] + arr[3]) + (10*arr[1] + arr[2]);
+        // int res = min(sum1, sum2);
+        // return res;
+        
+        string s = to_string(num);
+        sort(s.begin(), s.end());
+        int res = (s[0] - '0' + s[1] - '0') * 10 + s[2] - '0' + s[3] - '0';
         return res;
     }
 };
