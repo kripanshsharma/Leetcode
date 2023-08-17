30c54d641289530db6557717352883108f264abd
Kripansh Sharma <72396861+kripanshsharma@users.noreply.github.com>
2023-08-18 04:15:11 +0530

    Time: 2 ms (40.10%) | Memory: 5.9 MB (58.87%) - LeetSync

--- 2264-minimum-sum-of-four-digit-number-after-splitting-digits/minimum-sum-of-four-digit-number-after-splitting-digits.cpp
+++ 2264-minimum-sum-of-four-digit-number-after-splitting-digits/minimum-sum-of-four-digit-number-after-splitting-digits.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
     int minimumSum(int num) {
+        // vector<int> arr;
+        // while(num!=0){
+        //     arr.push_back(num%10);
+        //     num = num/10;
+        // }
+        // sort(arr.begin(), arr.end());
+        // int sum1 = (10*arr[0] + arr[2]) + (10*arr[1] + arr[3]);
+        // int sum2 = (10*arr[0] + arr[3]) + (10*arr[1] + arr[2]);
+        // int res = min(sum1, sum2);
+        // return res;
         
         string s = to_string(num);
         sort(s.begin(), s.end());
