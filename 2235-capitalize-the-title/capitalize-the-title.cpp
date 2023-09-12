28fb741aaf4deb0fa52ec938eb434ef190fb05e5
Kripansh Sharma <72396861+kripanshsharma@users.noreply.github.com>
2023-09-13 00:15:52 +0530

    Time: 0 ms (100.00%) | Memory: 6.2 MB (76.68%) - LeetSync

--- 2235-capitalize-the-title/capitalize-the-title.cpp
+++ 2235-capitalize-the-title/capitalize-the-title.cpp
@@ -15,37 +15,5 @@ public:
             i++;
         }
         return s;
-        // int cnt=0,j;
-        // for(j=0; j!=' '; j++){
-        //     cnt++;
-        // }
-        // if(cnt>=3){
-        //     title[0]= toupper(title[0]);
-            
-        // }
-        // else{
-        //     title[0]= tolower(title[0]);
-        // }
-        // for(int i=1; i<title.size(); i++){
-        //     if(title[i]==' '){
-        //         int c=0;
-        //         for(int k=i+1; k!=' '; k++){
-        //             c++;
-        //         }
-        //         if(c>=3){
-        //             title[i+1]= toupper(title[i+1]);
-        //             i++;
-        //         }
-        //         else{
-        //             title[i+1]= tolower(title[i+1]);
-        //             i++;
-        //         }
-        //     }
-        //     else{
-        //         title[i]= tolower(title[i]);
-        //     }
-        // }
-        // return title;
-
     }
 };
