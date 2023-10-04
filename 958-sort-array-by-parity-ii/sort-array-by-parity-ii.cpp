c4b4f7425a41cb389e1f7b3e065cacc707af8638
Kripansh Sharma <72396861+kripanshsharma@users.noreply.github.com>
2023-10-05 02:47:20 +0530

    Time: 16 ms (53.34%) | Memory: 24.4 MB (5.79%) - LeetSync

--- 958-sort-array-by-parity-ii/sort-array-by-parity-ii.cpp
+++ 958-sort-array-by-parity-ii/sort-array-by-parity-ii.cpp
@@ -1,29 +1,54 @@
 class Solution {
 public:
     vector<int> sortArrayByParityII(vector<int>& s) {
-        int n= s.size();
-        priority_queue<int> evenheap;
-        priority_queue <int> oddheap;
-        for(int i=0; i<n;i++){
-            if(s[i]%2==0 && i%2!=0){
-                evenheap.push(s[i]);
+        //int n= s.size();
+        // priority_queue<int> evenheap;
+        // priority_queue <int> oddheap;
+        // for(int i=0; i<n;i++){
+        //     if(s[i]%2==0 && i%2!=0){
+        //         evenheap.push(s[i]);
+        //     }
+        //     else if(s[i]%2!=0 && i%2==0){
+        //         oddheap.push(s[i]);
+        //     }
+        // }
+        // int i = 0;
+        // while(i<n){
+        //     if(s[i]%2!=0 && i%2==0){
+        //         s[i] = evenheap.top();
+        //         evenheap.pop();
+        //     }
+        //     else if(s[i]%2==0 && i%2!=0){
+        //         s[i] = oddheap.top();
+        //         oddheap.pop();
+        //     }
+        //     i++;
+        // }
+        vector<int>even;
+        vector<int>odd;
+        vector<int>v;
+        int n=s.size();
+        for(int i=0;i<n;i++)
+        {
+            if(s[i]%2==0) even.push_back(s[i]);
+            else odd.push_back(s[i]);
+        }
+        int k=0;
+        int l=0;
+        for(int i=0;i<n;i++)
+        {
+            if(i%2==0) 
+            {
+                v.push_back(even[k]);
+                k++;
             }
-            else if(s[i]%2!=0 && i%2==0){
-                oddheap.push(s[i]);
+            else
+            {
+                v.push_back(odd[l]);
+                l++;
             }
         }
-        int i = 0;
-        while(i<n){
-            if(s[i]%2!=0 && i%2==0){
-                s[i] = evenheap.top();
-                evenheap.pop();
-            }
-            else if(s[i]%2==0 && i%2!=0){
-                s[i] = oddheap.top();
-                oddheap.pop();
-            }
-            i++;
-        }
-        return s;
+        return v;
+        //return s;
     }
 };
