82e5a6060092c204e4bb89a5b3fe13df8a76eb4d
Kripansh Sharma <72396861+kripanshsharma@users.noreply.github.com>
2023-10-10 21:48:27 +0530

    Time: 153 ms (71.13%) | Memory: 64.5 MB (71.55%) - LeetSync

---
+++ 2119-minimum-number-of-operations-to-make-array-continuous/minimum-number-of-operations-to-make-array-continuous.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int uniqueLen = 1;
+        for (int i = 1; i < nums.size(); ++i) {
+            if (nums[i] != nums[i - 1]) {
+                nums[uniqueLen++] = nums[i];
+            }
+        }
+
+        int ans = nums.size();
+        for (int i = 0, j = 0; i < uniqueLen; ++i) {
+            while (j < uniqueLen && nums[j] - nums[i] <= nums.size() - 1) {
+                ++j;
+            }
+            ans = min(ans, static_cast<int>(nums.size()) - (j - i));
+        }
+
+        return ans;
+    
+
+    }
+};
