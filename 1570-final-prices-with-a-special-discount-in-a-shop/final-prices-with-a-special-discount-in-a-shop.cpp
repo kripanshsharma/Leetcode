0a9170b42e30da519581491bcabf4d97ffb7da45
Kripansh Sharma <72396861+kripanshsharma@users.noreply.github.com>
2023-10-18 23:56:33 +0530

    Time: 4 ms (44.20%) | Memory: 10.6 MB (8.29%) - LeetSync

---
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> finalPrices(vector<int>& prices) {
+        // if next greater smaller for such question stack works to get in O(n)time
+        stack<int>st;
+        vector<int>res(prices.size());
+        for(int i = prices.size()-1 ;i>=0;i--){
+            while(st.empty()==false&&st.top()>prices[i]){
+                st.pop();
+            }
+            if(st.empty()){
+                res[i] = prices[i];
+            }else res[i] = prices[i]-st.top();
+            st.push(prices[i]);
+        }
+        return res ;
+    }
+};
