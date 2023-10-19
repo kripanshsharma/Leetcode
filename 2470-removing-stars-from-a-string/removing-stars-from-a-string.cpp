04990cf9d735e7bf6bcb16edaf2745caacfc0891
Kripansh Sharma <72396861+kripanshsharma@users.noreply.github.com>
2023-10-19 22:30:13 +0530

    Time: 83 ms (78.71%) | Memory: 26.2 MB (55.15%) - LeetSync

---
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string removeStars(string s) {
+        string k="";
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='*'){
+                k.pop_back();
+            }
+            else{
+                k.push_back(s[i]);
+            }
+        }
+        return k;
+    }
+};
