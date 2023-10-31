8155d5df60819e149ce3de4d8e834eb9cf705977
Kripansh Sharma <72396861+kripanshsharma@users.noreply.github.com>
2023-10-31 21:08:45 +0530

    Time: 13 ms (20.62%) | Memory: 11.1 MB (17.73%) - LeetSync

---
+++ 260-single-number-iii/single-number-iii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        for(int i=0; i<nums.size(); i++){
+            mp[nums[i]]++;
+        }
+        vector<int> ans;
+        for(auto &it:mp){
+            if(it.second==1){
+
+                ans.push_back(it.first);
+                if(ans.size()==2){
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
