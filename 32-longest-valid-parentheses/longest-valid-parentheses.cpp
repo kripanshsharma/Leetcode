e956901d209d63ae4e3c88a882a01993d147d115
Kripansh Sharma <72396861+kripanshsharma@users.noreply.github.com>
2024-01-05 10:55:39 +0530

    Time: 3 ms (79.14%) | Memory: 7.6 MB (79.27%) - LeetSync

---
+++ 32-longest-valid-parentheses/longest-valid-parentheses.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        stack<int> st;
+        st.push(-1);
+        
+        int maxLen = 0;
+
+        for(int i = 0; i < s.size(); i++) {
+            char ch = s[i];
+
+            if(ch == '(') {
+                st.push(i);
+            }
+
+            else {
+                st.pop();
+                if(st.empty()) {
+                    st.push(i);
+                }
+                else {
+                    int len = i - st.top();
+                    maxLen = max(len, maxLen);
+                }
+            }
+        }
+        return maxLen;
+    }
+};
