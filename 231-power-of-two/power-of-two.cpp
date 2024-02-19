f5a5abd282e676d02ba5919e4374fcd94f26bffd
Kripansh Sharma <72396861+kripanshsharma@users.noreply.github.com>
2024-02-20 01:07:41 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (19.56%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        // int b1 = n||(n-1);
-        // int b2 = n^(n-1);
-        if(n==0 || n==INT_MIN) return false;
+         if(n==0 || n==INT_MIN) return false;
         if((n&(n-1))==0){
             return true;
         }
