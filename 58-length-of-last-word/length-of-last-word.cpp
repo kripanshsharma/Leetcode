0747a9a701adfe0d4eaf286f0da9521ea186eeac
Kripansh Sharma <72396861+kripanshsharma@users.noreply.github.com>
2024-04-01 11:54:19 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (87.89%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.cpp
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
-    int lengthOfLastWord(string s){
-        int n = s.size();
-        int cnt =0, flag=0;
-        for(int i = n-1; i>=0; i--){
-
-            if(s[i]!=' '){
-                flag =1;
-                cnt++;
-            }
-            else if(s[i]==' ' && flag){
+    int lengthOfLastWord(string s) {
+        int n=s.size();
+        int f;
+        char p = ' ';
+        for(int i=n-1;i>=0;i--){
+            if(s[i] != p){
+                f=i;
                 break;
             }
         }
-        return cnt;
+        int count=0;
+        for(int i=f; i>=0; i--){
+            if(s[i] == p)
+            break;
+
+            count++;
+        }
+
+        return count;
     }
 };
